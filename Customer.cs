63a529d8808bea689c3a33295f60516bfbb6fd08
Z_M_M <Z_M_M@DESKTOP-0J9GSLJ>
2024-10-16 23:00:03 +0000

    Oct 17, 2024, 2:00 AM

---
+++ Customer.cs
@@ -0,0 +1,10 @@
+namespace Customer{
+    public class CustomerService 
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+        public int salary { get; set; }
+
+    }
+}
