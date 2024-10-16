ef3ba806200aaf189fb28c0c6ab0b34fbd066051
Z_M_M <Z_M_M@DESKTOP-0J9GSLJ>
2024-10-16 23:43:21 +0000

    Oct 17, 2024, 2:43 AM

--- Program.cs
+++ Program.cs
@@ -169,6 +169,41 @@ public class customr{
         throw new System.NotImplementedException();
         return base.GetHashCode();
     }
+    
+    public static  class DataSeed{
+        public static List<CustomerService> addData(){
+            List<CustomerService> i=new List<CustomerService>();i\
+            i.Add(new CustomerService{
+                CustomerID = 2,
+                Name ="Zikom daim",
+                salary=12,
+                description="My jop in information System"
+
+
+            
+            });
+            i.Add(new CustomerService{
+                CustomerID = 2,
+                Name ="Zikom Ali",
+                salary=12,
+                description="My jop in analysis System"
+
+
+            
+            });
+            i.Add(new CustomerService{
+                CustomerID = 2,
+                Name ="Zikom mansoor",
+                salary=1,
+                description="Data mining "
+
+
+            
+            });
+
+        }
+    }
+
 
 /**************************************************************/
 /**************************************************************/
