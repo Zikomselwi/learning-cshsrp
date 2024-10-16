ba4321d2edd6f4e131c3c4887ca00cca3c03e88b
Z_M_M <Z_M_M@DESKTOP-0J9GSLJ>
2024-10-16 23:24:47 +0000

    Oct 17, 2024, 2:24 AM

--- DataSeed.cs
+++ DataSeed.cs
@@ -2,7 +2,33 @@ using namespace seed{
     public static  class getData{
         public static List<CustomerService> addData(){
             List<CustomerService> i=new List<CustomerService>();i\
-            i.Add(new CustomerService("","",))
+            i.Add(new CustomerService{
+                CustomerID = 2,
+                Name ="Zikom daim",
+                salary=12,
+                description="My jop in information System"
+
+
+            
+            });
+            i.Add(new CustomerService{
+                CustomerID = 2,
+                Name ="Zikom Ali",
+                salary=12,
+                description="My jop in analysis System"
+
+
+            
+            });
+            i.Add(new CustomerService{
+                CustomerID = 2,
+                Name ="Zikom mansoor",
+                salary=1,
+                description="Data mining "
+
+
+            
+            });
 
         }
     }
